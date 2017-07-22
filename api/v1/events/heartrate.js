65fa8357316ff37ed08c96e2f5e1f6c6ac87fb7e
Maxwell Carter <max4carter@gmail.com>
2017-07-22 10:05:44 -0400

    Updated heartrate event to follow database model

--- api/v1/events/heartrate.js
+++ api/v1/events/heartrate.js
@@ -3,16 +3,31 @@ var ctrls = require('../controllers');
 
 module.exports = (socket) => {
     log.info('Initializing Event [heartrate]');
-    var collection = 'heartrate';
 
     socket.on('heartrate', (data) => {
         log.info('Event [heartrate]');
         log.info(data);
 
-        ctrls.database.insert(collection, data);
+        if (!data || (Object.keys(data).length === 0 && data.constructor === Object)) {
+            log.error('Event payload is empty');
+            return;
+        }
 
-        // TODO: Data Validation
-        // TODO: Perform anomaly analysis
-        // TODO: Save to database
+        if (!('device' in data) || !('bpm' in data)) {
+            log.error('Missing required event payload');
+            return;
+        }
+
+        ctrls.database.read('devices', data.device).then((result) => {
+            let obj = {
+                bpm: data.bpm,
+                timestamp: Date.now()
+            };
+            let collection = 'heartrate/' + result.baby;
+            ctrls.database.insert(collection, data);
+            // TODO: Perform anomaly analysis
+        }).catch((error) => {
+            log.error('Could not save heartrate data!');
+        });
     });
 };
