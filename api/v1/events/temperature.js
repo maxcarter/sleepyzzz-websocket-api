e012933fdde27796dc4845a975dd861743669d6e
Maxwell Carter <max4carter@gmail.com>
2017-07-22 10:35:29 -0400

    Implemented temperature event

--- api/v1/events/temperature.js
+++ api/v1/events/temperature.js
@@ -7,5 +7,27 @@ module.exports = (socket) => {
     socket.on('temperature', (data) => {
         log.info('Event [temperature]');
         log.info(data);
+
+        if (!data || (Object.keys(data).length === 0 && data.constructor === Object)) {
+            log.error('Event payload is empty');
+            return;
+        }
+
+        if (!('device' in data) || !('temperature' in data)) {
+            log.error('Missing required event payload');
+            return;
+        }
+
+        ctrls.database.read('devices', data.device).then((result) => {
+            let obj = {
+                temperature: data.temperature,
+                timestamp: Date.now()
+            };
+            let collection = 'temperature/' + result.baby;
+            ctrls.database.insert(collection, obj);
+            // TODO: Perform anomaly analysis
+        }).catch((error) => {
+            log.error('Could not save temperature data!');
+        });
     });
 };
