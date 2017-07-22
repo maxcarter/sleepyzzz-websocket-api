81ec99571d9a3acba0db823a04bce8b24aa11b3f
Maxwell Carter <max4carter@gmail.com>
2017-07-22 10:58:46 -0400

    Implemented battery event

--- api/v1/events/battery.js
+++ api/v1/events/battery.js
@@ -7,5 +7,26 @@ module.exports = (socket) => {
     socket.on('battery', (data) => {
         log.info('Event [battery]');
         log.info(data);
+
+        if (!data || (Object.keys(data).length === 0 && data.constructor === Object)) {
+            log.error('Event payload is empty');
+            return;
+        }
+
+        if (!('device' in data) || !('percentage' in data)) {
+            log.error('Missing required event payload');
+            return;
+        }
+
+        let obj = {
+            percentage: data.percentage,
+            timestamp: Date.now()
+        };
+
+        let collection = 'battery/' + data.device;
+        
+        ctrls.database.insert(collection, obj).catch((error) => {
+            log.error('Could not save battery percentage');
+        });
     });
 };
