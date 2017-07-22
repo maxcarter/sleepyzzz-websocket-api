3d111dc0c00aba962edbed8f5b0410cb5c36abb6
Maxwell Carter <max4carter@gmail.com>
2017-07-22 10:48:39 -0400

    Implemented movement event

--- api/v1/events/movement.js
+++ api/v1/events/movement.js
@@ -7,5 +7,29 @@ module.exports = (socket) => {
     socket.on('movement', (data) => {
         log.info('Event [movement]');
         log.info(data);
+
+        if (!data || (Object.keys(data).length === 0 && data.constructor === Object)) {
+            log.error('Event payload is empty');
+            return;
+        }
+
+        if (!('device' in data) || !('yaw' in data) || !('pitch' in data) || !('roll' in data)) {
+            log.error('Missing required event payload');
+            return;
+        }
+
+        ctrls.database.read('devices', data.device).then((result) => {
+            let obj = {
+                yaw: data.yaw,
+                pitch: data.pitch,
+                roll: data.roll,
+                timestamp: Date.now()
+            };
+            let collection = 'movement/' + result.baby;
+            ctrls.database.insert(collection, obj);
+            // TODO: Perform anomaly analysis
+        }).catch((error) => {
+            log.error('Could not save movement data!');
+        });
     });
 };
