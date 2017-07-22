ce6927ab7f1cb10a407068b3e3d22f7b186b1d9d
Maxwell Carter <max4carter@gmail.com>
2017-07-22 09:05:43 -0400

    Refactored database wrapper to use promises

--- api/v1/controllers/database.js
+++ api/v1/controllers/database.js
@@ -1,26 +1,25 @@
 const log = require('winston');
 const firebase = require('firebase');
+const q = require('q');
 const config = require('../../../config');
 var db = config[config.mode].database.node;
 
-// var ref = firebase.database().ref('sleepyzzz-dev');
-//
-// var messageRef = ref.child('messages');
-// var messageRef = messageRef.push();
-// console.log('here');
-// messageRef.set({
-//     name: 'max'
-// });
 module.exports = {
     insert: (collection, data) => {
         log.info('Controller [database] Function [insert]');
         log.info(`Inserting data into Firebase [${collection}]`);
+
+        let deferred = q.defer();
         let ref = firebase.database().ref(db);
         let collectionRef = ref.child(collection).push();
         collectionRef.set(data).then(() => {
             log.info(`Successfully inserted data into Firebase [${collection}]`);
+            deferred.resolve();
         }).catch((error) => {
             log.error(`Failed Inserting data into Firebase [${collection}]`, error);
+            deferred.reject(error);
         });
+
+        return deferred.promise;
     }
 };
